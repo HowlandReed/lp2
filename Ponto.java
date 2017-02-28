5906c0a11a12c641d1d8a6e6cc561ff5b2bf808c
Matheus Nascimento <matheusnascimentocefet@gmail.com>
2017-02-28 18:46:41 -0300

    Update Ponto.java

--- Ponto.java
+++ Ponto.java
@@ -1,18 +1,18 @@
 //Matheus Nascimento
 public class Ponto{
 	private  double x, y;
-		public void setX(double x){
-			this.x = x;
-		}
-		public  double getX(){
-			return x;
-		}
-		public void setY(double y){
-			this.y = y;
-		}
-		public double getY(){
-			return y;
-		}
+	public void setX(double x){
+		this.x = x;
+	}
+	public  double getX(){
+		return x;
+	}
+	public void setY(double y){
+		this.y = y;
+	}
+	public double getY(){
+		return y;
+	}
 	public static void main(String []args){
 		Ponto p1 = new Ponto();
 		p1.setX(10);
